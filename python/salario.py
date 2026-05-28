6622f16ba942a5ba226d796d06578100bc71b458
Joownlok <joao.6564693@gmail.com>
2026-05-28 20:36:55 -0300

    ex salario

---
+++ python/salario.py
@@ -0,0 +1,7 @@
+print('-' * 30)
+valor_hora = float(input("Digite o valor da hora trabalhada: "))
+horas_trabalhadas = float(input("Digite o número de horas trabalhadas no mês: "))
+print('-' * 30)
+salario = valor_hora * horas_trabalhadas
+print(f'O salário mensal é igual a R$ {salario:.2f}')
+print('-' * 30)
